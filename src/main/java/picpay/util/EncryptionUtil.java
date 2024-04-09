84228c6fe93502c4287246cf26e104d1941aa640
unknown <gbferreira08@gmail.com>
2024-04-09 15:23:56 -0300

    encrypto

--- src/main/java/picpay/util/EncryptionUtil.java
+++ src/main/java/picpay/util/EncryptionUtil.java
@@ -1,4 +1,22 @@
 package picpay.util;
 
+import picpay.exception.ApplicationException;
+
+import java.security.MessageDigest;
+
 public class EncryptionUtil {
+    private String generateHashPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(password.getBytes());
+            StringBuilder hexString = new StringBuilder();
+
+            for (byte b : hash) {
+                hexString.append(String.format("%02x", b));
+            }
+            return hexString.toString();
+        } catch (Exception ex) {
+            throw new ApplicationException("falha ao gerar senha");
+        }
+    }
 }
